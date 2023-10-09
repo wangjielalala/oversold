956c316fecc1064be643a7f20b3be98e1dbbe9a6
wangjie1 <wangjie1@akulaku.com>
2023-10-09 19:24:42 +0800

    cpu 超卖算法调整

--- pkg/adfunc/func_Oversold_node_test.go
+++ pkg/adfunc/func_Oversold_node_test.go
@@ -15,25 +15,3 @@ func TestFunc(t *testing.T) {
 
 	klog.Info(overcpu(Allcalalbe.Cpu(), "2"))
 }
-
-func TestAdd(t *testing.T) {
-	//tests := []struct {
-	//	a        resource.Quantity
-	//	b        resource.Quantity
-	//	expected resource.Quantity
-	//}{
-	//	{decQuantity(10, 0, resource.DecimalSI), decQuantity(1, 1, resource.DecimalSI), decQuantity(20, 0, resource.DecimalSI)},
-	//	{decQuantity(10, 0, resource.DecimalSI), decQuantity(1, 0, resource.BinarySI), decQuantity(11, 0, resource.DecimalSI)},
-	//	{decQuantity(10, 0, resource.BinarySI), decQuantity(1, 0, resource.DecimalSI), decQuantity(11, 0, resource.BinarySI)},
-	//	{resource.Quantity{Format: resource.DecimalSI}, decQuantity(50, 0, resource.DecimalSI), decQuantity(50, 0, resource.DecimalSI)},
-	//	{decQuantity(50, 0, resource.DecimalSI), resource.Quantity{Format: resource.DecimalSI}, decQuantity(50, 0, resource.DecimalSI)},
-	//	{resource.Quantity{Format: resource.DecimalSI}, resource.Quantity{Format: resource.DecimalSI}, decQuantity(0, 0, resource.DecimalSI)},
-	//}
-	//
-	//for i, test := range tests {
-	//	test.a.Add(test.b)
-	//	if test.a.Cmp(test.expected) != 0 {
-	//		t.Errorf("[%d] Expected %q, got %q", i, test.expected.String(), test.a.String())
-	//	}
-	//}
-}
